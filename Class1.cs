bff9ca65617299f3467699eee102f3c5528b5eb1
ahmad <bha296780@gmail.com>
2022-09-29 16:25:21 -0400

    final

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Pattern
+{
+    public class VehicleFactory
+    {
+        public static IVehicle BuildCar(int user)
+        {
+            if (user == 4)
+            {
+                return new Car();
+            }
+            else if (user == 2)
+            {
+                return new Motorcycle();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    public interface IVehicle
+    {
+        public void drive();
+    }
+    public class Car : IVehicle
+    {
+        public void drive()
+        {
+            Console.WriteLine("here is your new car"); ;
+        }
+    }
+    public class Motorcycle : IVehicle
+    {
+        public void drive()
+        {
+            Console.WriteLine("here is your new motorcycle"); ;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Factory_Pattern;
+using System.Data.SqlTypes;
+
+Console.WriteLine("How many tires on vehicle?");
+var user = Int32.Parse(Console.ReadLine());
+
+IVehicle vehicle = VehicleFactory.BuildCar(user);
+vehicle.drive();
+Console.ReadLine();
